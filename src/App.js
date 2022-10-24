fa10a9010ae9bf68152471e0bdf95080ee329e92
hasansheikh1 <hasanfaysal17@gmail.com>
2022-10-24 20:18:35 +0500

    finalizing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,35 @@ function App() {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
 
+  
+  useEffect(() => {
+
+    function getTrending() {
+      const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news',
+        params: {safeSearch: 'Off', textFormat: 'Raw'},
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': '1cede07cd8mshdfa8ae0bcfae0ccp11f8fcjsnbc26c5e53856',
+          'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+        }
+      };
+  
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+          setData(response.data.value);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  
+    getTrending();
+  }, []);
+
   const getNews = (e) => {
     // axios.get("");
     e.preventDefault();
@@ -50,31 +79,47 @@ function App() {
           placeholder="Enter City.."
         /> */}
 
-<div class="wrap">
-   <div class="search">
-      <input id="cityName" onChange={(e)=>{
-        
-        setQuery(e.target.value);
-
-      }} type="text" className="searchTerm" placeholder="What are you looking for?"/>
-      <button type="submit" className="searchButton">
-        Search!
-     </button>
-   </div>
-</div>
-
-
-        <div>
-          {data.map((eachPost) => (
-            <div className="post" key={eachPost.name}>
-              <h1>{eachPost.name}</h1>
-              <span>{moment(eachPost.datePublished).format('MMMM Do YYYY, h:mm: a')}</span>
-              <h3>{eachPost.description}</h3>
-
-              <button className="btn-read" ><a className="btn-read1" target="_blank" href={eachPost.url}>Read More</a></button>
-            </div>
-          ))}
+        <div className="wrap">
+          <div className="search">
+            <input
+              id="cityName"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+              type="text"
+              className="searchTerm"
+              placeholder="What are you looking for?"
+            />
+            <button type="submit" className="searchButton">
+              Search!
+            </button>
+          </div>
         </div>
+
+        {/* <div> */}
+        {data.map((eachPost) => (
+          <div className="post" key={eachPost.name}>
+            <h1>{eachPost?.name}</h1>
+            <img
+              src={eachPost?.image?.thumbnail?.contentUrl
+                .replace("&pid=News", "")
+                .replace("pid=News&", "")
+                .replace("pid=News", "")}
+              alt="image"
+            />
+            <span>
+              {moment(eachPost?.datePublished).format("MMMM Do YYYY, h:mm a")}
+            </span>
+            <h3>{eachPost?.description}</h3>
+
+            <button className="btn-read">
+              <a className="btn-read1" target="_blank" href={eachPost.url}>
+                Read More
+              </a>
+            </button>
+          </div>
+        ))}
+        {/* </div> */}
       </form>
     </div>
   );
